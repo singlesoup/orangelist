c6a3042c668dd437ec12aa95a304ab44823b18e9
Himanshu Nawalkar <himanshu.nawalkar@gmail.com>
2024-08-16 14:57:07 +0530

    Setting Page: Partial

--- lib/src/home/widgets/about_banner.dart
+++ lib/src/home/widgets/about_banner.dart
@@ -1,17 +1,28 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart' show IconButton, MaterialPageRoute;
 import 'package:flutter/widgets.dart'
     show
+        Align,
+        Alignment,
         BorderRadius,
         BoxDecoration,
         BuildContext,
-        Column,
         Container,
-        CrossAxisAlignment,
         EdgeInsets,
+        Flexible,
+        Navigator,
         Radius,
-        SizedBox,
+        Row,
+        Spacer,
         StatelessWidget,
         Text,
         Widget;
+import 'package:font_awesome_flutter/font_awesome_flutter.dart'
+    show FaIcon, FontAwesomeIcons;
+import 'package:orangelist/src/home/screens/settingsPage/screens/settings.dart'
+    show SettingsPage;
+import 'package:orangelist/src/home/widgets/web_app_outline_widget.dart'
+    show WebAppOutlineWidget;
 import 'package:orangelist/src/theme/colors.dart' show sandAccent;
 import 'package:orangelist/src/theme/text_theme.dart' show sfTextTheme;
 
@@ -37,21 +48,33 @@ class AboutBanner extends StatelessWidget {
         right: 14,
         top: 12,
       ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
+      child: Row(
         children: [
+          const Spacer(),
           Text(
             'Orangelist',
-            style: sfTextTheme.labelLarge,
+            style: sfTextTheme.titleLarge,
           ),
-          const SizedBox(
-            height: 4,
+          Flexible(
+            child: Align(
+              alignment: Alignment.centerRight,
+              child: IconButton(
+                onPressed: () {
+                  Navigator.of(context).push(
+                    MaterialPageRoute<void>(
+                      builder: (BuildContext context) => kIsWeb
+                          ? const WebAppOutlineWidget(child: SettingsPage())
+                          : const SettingsPage(),
+                    ),
+                  );
+                },
+                icon: const FaIcon(
+                  FontAwesomeIcons.gear,
+                  color: sandAccent,
+                ),
+              ),
+            ),
           ),
-          Text(
-            'by Soup',
-            style: sfTextTheme.bodyMedium,
-          ),
-          // Todo: Add settings flow as Icon and navigation_utils or auto_route in row
         ],
       ),
     );
